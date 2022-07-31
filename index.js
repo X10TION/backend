8c0126df40b11f40f8d6458872d1ed6bcb43a013
Joseph Christopher Jacob <36853161+X10TION@users.noreply.github.com>
2022-07-30 22:31:40 -0400

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,40 @@
+/*
+Author : CHRISTOPHER JOSEPH 
+Description: CONNECTED EDUCATION SOFTWare
+Date: feb -12-2022
+*/
+
+const express = require('express')
+const mongoose = require('mongoose')
+const router = require('./routes/user-routes')
+const public = require('./routes/publicResource')
+const courses = require('./routes/courses-routes')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+require('dotenv').config()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/uploads', express.static('uploads'))
+app.use(cookieParser())
+app.use(cors())
+/*
+*/
+app.use('/api/v1', courses)
+app.use('/api/v1', router)
+app.use('/api/v1', public)
+
+//
+mongoose.connect("mongodb+srv://MAUCES:MAUCES@cluster0.bwdex.mongodb.net/?retryWrites=true&w=majority")
+.then(() =>{
+    app.listen(5000, () => {
+        console.log("Database is initializing  from mongodb cloud..")
+        console.log("Server is initializing  to port 500...")
+    })
+}).catch((err) =>{
+    console.log("ERROR CONNECTING TO THE SERVER :" + err)
+})
+
+
